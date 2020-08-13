bb69b3b16ac8e4a3687f20bd315554148cfc1f73
gurumur2 <SARkav1213@>
2020-08-13 17:41:01 -0400

    Changed code to allow users to customise the cards

--- pub/js/example.js
+++ pub/js/example.js
@@ -1,38 +1,170 @@
 const s = new Card();
-s.makeCardANone(
-  "card",
-  "./images/sargu4.jpg",
-  "The library and this website were designed by Sargunan Gurumurthy."
-);
-s.makeCardAOpacity(
-  "card2",
-  "./images/sargu2.jpg",
+// s.makeCard("card", 2, 1, [
+//   "./images/sargu4.jpg",
+//   "The library and this website were designed by Sargunan Gurumurthy.",
+// ]);
+// s.makeCard("card2", 2, 2, [
+//   "./images/sargu2.jpg",
+//   "./images/sargu.jpg",
+//   "Sargunan",
+//   "WEB APP DESIGNER",
+//   [
+//     "https://www.facebook.com/sargu.sargu/",
+//     "https://github.com/Sargunan1213",
+//     "https://www.linkedin.com/in/sargu/",
+//   ],
+// ]);
+// s.makeCard("card3", 2, 3, [
+//   "./images/sargu7.jpg",
+//   "Sargunan",
+//   "WEB APP DESIGNER",
+// ]);
+// s.makeCard("card4", 2, 4, [
+//   "http://techslides.com/demos/sample-videos/small.mp4",
+//   "video/mp4",
+//   "./images/sargu.jpg",
+//   "Sargunan",
+//   [
+//     "https://www.facebook.com/sargu.sargu/",
+//     "https://github.com/Sargunan1213",
+//     "https://www.linkedin.com/in/sargu/",
+//     "#",
+//     "https://www.instagram.com/sarguskillzzz",
+//     "mailto:sargunan1213@gmail.com",
+//   ],
+// ]);
+
+/*
+
+
+
+
+
+
+
+
+*/
+
+// s.makeCard(
+//   "card",
+//   4,
+//   [1, 2],
+//   [
+//     [
+//       "./images/sargu4.jpg",
+//       "The library and this website were designed by Sargunan Gurumurthy.",
+//     ],
+//     [
+//       "./images/sargu2.jpg",
+//       "./images/sargu.jpg",
+//       "Sargunan",
+//       "WEB APP DESIGNER",
+//       [
+//         "https://www.facebook.com/sargu.sargu/",
+//         "https://github.com/Sargunan1213",
+//         "https://www.linkedin.com/in/sargu/",
+//       ],
+//     ],
+//   ]
+// );
+// s.makeCard(
+//   "card2",
+//   4,
+//   [2, 1],
+//   [
+//     [
+//       "./images/sargu2.jpg",
+//       "./images/sargu.jpg",
+//       "Sargunan",
+//       "WEB APP DESIGNER",
+//       [
+//         "https://www.facebook.com/sargu.sargu/",
+//         "https://github.com/Sargunan1213",
+//         "https://www.linkedin.com/in/sargu/",
+//       ],
+//     ],
+//     [
+//       "./images/sargu4.jpg",
+//       "The library and this website were designed by Sargunan Gurumurthy.",
+//     ],
+//   ]
+// );
+// s.makeCard(
+//   "card3",
+//   4,
+//   [2, 3],
+//   [
+//     [
+//       "./images/sargu2.jpg",
+//       "./images/sargu.jpg",
+//       "Sargunan",
+//       "WEB APP DESIGNER",
+//       [
+//         "https://www.facebook.com/sargu.sargu/",
+//         "https://github.com/Sargunan1213",
+//         "https://www.linkedin.com/in/sargu/",
+//       ],
+//     ],
+//     ["./images/sargu7.jpg", "Sargunan", "WEB APP DESIGNER"],
+//   ]
+// );
+// s.makeCard(
+//   "card4",
+//   4,
+//   [3, 4],
+//   [
+//     ["./images/sargu7.jpg", "Sargunan", "WEB APP DESIGNER"],
+//     [
+//       "http://techslides.com/demos/sample-videos/small.mp4",
+//       "video/mp4",
+//       "./images/sargu.jpg",
+//       "Sargunan",
+//       [
+//         "https://www.facebook.com/sargu.sargu/",
+//         "https://github.com/Sargunan1213",
+//         "https://www.linkedin.com/in/sargu/",
+//         "#",
+//         "https://www.instagram.com/sarguskillzzz",
+//         "mailto:sargunan1213@gmail.com",
+//       ],
+//     ],
+//   ]
+// );
+s.makeSlider("card", 1, [
   "./images/sargu.jpg",
-  "Sargunan",
-  "WEB APP DESIGNER",
-  [
-    "https://www.facebook.com/sargu.sargu/",
-    "https://github.com/Sargunan1213",
-    "https://www.linkedin.com/in/sargu/",
-  ]
-);
-s.makeCardAFlip(
-  "card3",
-  "./images/sargu7.jpg",
-  "./images/sargu5.jpg",
-  "Sargunan",
-  "WEB APP DESIGNER",
-  [
-    "https://www.facebook.com/sargu.sargu/",
-    "https://github.com/Sargunan1213",
-    "https://www.linkedin.com/in/sargu/",
-    "#",
-    "https://www.instagram.com/sarguskillzzz",
-    "mailto:sargunan1213@gmail.com",
-  ]
-);
-s.makeSliderOne("card4", [
-  "./images/sargu6.jpg",
+  "./images/sargu2.jpg",
   "./images/sargu3.jpg",
+  "./images/sargu4.jpg",
+  "./images/sargu5.jpg",
+  "./images/sargu6.jpg",
+  "./images/sargu7.jpg",
+]);
+s.makeSlider("card2", 1, [
+  "./images/sargu.jpg",
+  "./images/sargu2.jpg",
+  "./images/sargu3.jpg",
+  "./images/sargu4.jpg",
+  "./images/sargu5.jpg",
+  "./images/sargu6.jpg",
+  "./images/sargu7.jpg",
+]);
+
+s.makeSlider("card3", 1, [
+  "./images/sargu.jpg",
+  "./images/sargu2.jpg",
+  "./images/sargu3.jpg",
+  "./images/sargu4.jpg",
+  "./images/sargu5.jpg",
+  "./images/sargu6.jpg",
+  "./images/sargu7.jpg",
+]);
+
+s.makeSlider("card4", 1, [
+  "./images/sargu.jpg",
+  "./images/sargu2.jpg",
+  "./images/sargu3.jpg",
+  "./images/sargu4.jpg",
+  "./images/sargu5.jpg",
+  "./images/sargu6.jpg",
   "./images/sargu7.jpg",
 ]);
--- pub/js/scl.js
+++ pub/js/scl.js
@@ -1,280 +1,306 @@
 "use strict";
 
+const default_image =
+  "https://res.cloudinary.com/doaxg76qe/image/upload/v1596985177/ben1xw6w8jfpaoltn1qz.png";
 function Card() {
   this.cards = [];
   this.slider = [];
 }
-let travel = 0;
+let travel = [];
+
 function tranRight(event) {
-  const container = document.getElementById("scl_Slider_imgholder");
+  const container = event.target.parentElement.childNodes[1];
+  // const container = document.getElementById("scl_Slider_imgholder");
+  // console.log(event);
   const count = container.childElementCount;
   const img_size = container.offsetWidth;
-  travel -= img_size;
-  if (travel <= count * img_size * -1) {
-    travel = 0;
+  const id = parseInt(container.id);
+  travel[id] -= img_size;
+  if (travel[id] <= count * img_size * -1) {
+    travel[id] = 0;
     container.style.transform = "translateX(0px)";
   } else {
-    container.style.transform = "translateX(" + travel + "px)";
+    container.style.transform = "translateX(" + travel[id] + "px)";
   }
 }
 function tranLeft(event) {
-  const container = document.getElementById("scl_Slider_imgholder");
+  // const container = document.getElementById("scl_Slider_imgholder");
+  const container = event.target.parentElement.childNodes[1];
   const count = container.childElementCount;
   const img_size = container.offsetWidth;
-  if (travel >= 0) {
-    travel = (count - 1) * img_size;
-    travel = travel * -1;
-    container.style.transform = "translateX(" + travel + "px)";
+  const id = parseInt(container.id);
+  if (travel[id] >= 0) {
+    travel[id] = (count - 1) * img_size;
+    travel[id] = travel[id] * -1;
+    container.style.transform = "translateX(" + travel[id] + "px)";
   } else {
-    travel += img_size;
-    container.style.transform = "translateX(" + travel + "px)";
+    travel[id] += img_size;
+    container.style.transform = "translateX(" + travel[id] + "px)";
   }
 }
 
-Card.prototype = {
-  makeCardANone: function (elementname, image_src, text) {
-    const card = document.createElement("div");
-    card.className = "scl_ANone";
-    const container = document.createElement("div");
-    container.className = "scl_ANone_container";
+function makeicon(face_div, link, icon) {
+  const div = document.createElement("div");
+  div.className = "scl_icondiv";
+  const facebook_a = document.createElement("a");
+  facebook_a.className = "scl_icon";
+  facebook_a.href = link;
+  facebook_a.target = "_blank";
+  const facebook_icon = document.createElement("i");
+  facebook_icon.className = icon;
+  facebook_icon.setAttribute("aria-hidden", "true");
+  facebook_a.appendChild(facebook_icon);
+  div.appendChild(facebook_a);
+  face_div.appendChild(div);
+}
+
+function card1(card, input) {
+  // input - image, text
+  const div = document.createElement("div");
+  div.className = "scl_card1_holder";
+  const container = document.createElement("div");
+  container.className = "scl_card1_container";
+  const img = document.createElement("img");
+  img.src = input[0];
+  img.className = "scl_card1_img";
+  container.appendChild(img);
+  div.appendChild(container);
+  const info = document.createElement("p");
+  info.appendChild(document.createTextNode(input[1]));
+  info.className = "scl_card1_text";
+  div.appendChild(info);
+  card.appendChild(div);
+}
+
+function card2(card, input) {
+  // input - image1, image2, titletext , text, links
+
+  //set 100% 100%
+  const div = document.createElement("div");
+  div.className = "scl_card2_holder";
+  const links = input[4];
+  const container = document.createElement("div");
+  container.className = "scl_card2_container";
+  const icon = document.createElement("div");
+  // icon.src = input[1];
+  icon.className = "scl_card2_icon";
+  icon.style.backgroundImage = "url(" + input[1] + ")";
+  container.appendChild(icon);
+  div.appendChild(container);
+
+  const container2 = document.createElement("div");
+  container2.className = "scl_card2_container2";
+  const info = document.createElement("h1");
+  info.appendChild(document.createTextNode(input[2]));
+  info.className = "scl_card2_text";
+  const description = document.createElement("p");
+  description.appendChild(document.createTextNode(input[3]));
+  description.className = "scl_card2_text2";
+
+  const face_div = document.createElement("div");
+  face_div.className = "scl_card2_aholder";
+
+  makeicon(face_div, links[0], "fa fa-facebook-square");
+  makeicon(face_div, links[1], "fa fa-github-square");
+  makeicon(face_div, links[2], "fa fa-linkedin-square");
+
+  container2.appendChild(info);
+  container2.appendChild(description);
+  container2.appendChild(face_div);
+  div.appendChild(container2);
+  div.style.backgroundImage = "url(" + input[0] + ")";
+  card.appendChild(div);
+}
+
+function card3(card, input) {
+  //input - image, text, text
+  // card.style.backgroundImage = "url(" + input[0] + ")";
+  const div = document.createElement("div");
+  div.className = "scl_card2_holder";
+  const text_container = document.createElement("div");
+  text_container.className = "scl_card3_container2";
+  const info = document.createElement("h1");
+  info.appendChild(document.createTextNode(input[1]));
+  info.className = "scl_card3_text";
+  const description = document.createElement("p");
+  description.appendChild(document.createTextNode(input[2]));
+  description.className = "scl_card3_text2";
+  text_container.appendChild(info);
+  text_container.appendChild(description);
+  div.appendChild(text_container);
+  div.style.backgroundImage = "url(" + input[0] + ")";
+  card.appendChild(div);
+}
+
+function card4(card, input) {
+  //input - video, type,  image, title, links
+  const div = document.createElement("div");
+  div.className = "scl_card4_holder";
+  const video = document.createElement("video");
+  video.className = "scl_card4_video";
+  video.loop = true;
+  video.autoplay = true;
+  video.muted = true;
+  const source = document.createElement("source");
+  source.src = input[0];
+  source.type = input[1];
+  video.appendChild(source);
+  div.appendChild(video);
+  const links = input[4];
+  const container = document.createElement("div");
+  container.className = "scl_card2_container";
+  const icon = document.createElement("div");
+  icon.className = "scl_card2_icon";
+  icon.style.backgroundImage = "url(" + input[2] + ")";
+  container.appendChild(icon);
+  div.appendChild(container);
+
+  const container2 = document.createElement("div");
+  container2.className = "scl_card2_container2";
+  const info = document.createElement("h1");
+  info.appendChild(document.createTextNode(input[3]));
+  info.className = "scl_card2_text";
+
+  const face_div = document.createElement("div");
+  face_div.className = "scl_card4_aholder";
+
+  makeicon(face_div, links[0], "fa fa-facebook-square");
+  makeicon(face_div, links[1], "fa fa-github-square");
+  makeicon(face_div, links[2], "fa fa-linkedin-square");
+
+  const face_div2 = document.createElement("div");
+  face_div2.className = "scl_card4_aholder";
+  makeicon(face_div2, links[3], "fa fa-twitter-square");
+  makeicon(face_div2, links[4], "fa fa-instagram");
+  makeicon(face_div2, links[5], "fa fa-envelope");
+
+  container2.appendChild(info);
+  container2.appendChild(face_div);
+  container2.appendChild(face_div2);
+  div.appendChild(container2);
+  // div.style.backgroundImage = "url(" + input[0] + ")";
+  card.appendChild(div);
+}
+
+const functions = {
+  1: card1,
+  2: card2,
+  3: card3,
+  4: card4,
+};
+
+function makeCardANone(elementname, facenumber, inputs) {
+  const card = document.createElement("div");
+  card.className = "scl_ANone";
+  functions[facenumber](card, inputs);
+  const elem = document.getElementsByName(elementname);
+  elem[0].appendChild(card);
+  return card;
+}
+
+function makeCardAOpacity(elementname, facenumber, inputs) {
+  const card = document.createElement("div");
+  card.className = "scl_AOpacity";
+  functions[facenumber](card, inputs);
+  const elem = document.getElementsByName(elementname);
+  elem[0].appendChild(card);
+  return card;
+}
+
+function makeCardAFlip(
+  elementname,
+  facenumber1,
+  inputs1,
+  facenumber2,
+  inputs2
+) {
+  const card = document.createElement("div");
+  card.className = "scl_AFlip";
+
+  const front = document.createElement("div");
+  front.className = "scl_AFlip_front";
+  functions[facenumber1](front, inputs1);
+  card.appendChild(front);
+
+  const back = document.createElement("div");
+  back.className = "scl_AFlip_back";
+  functions[facenumber2](back, inputs2);
+  card.appendChild(back);
+
+  const elem = document.getElementsByName(elementname);
+  elem[0].appendChild(card);
+  return card;
+}
+
+function makeSliderOne(elementname, image_list) {
+  const slider = document.createElement("div");
+  slider.className = "scl_Slider";
+
+  const left_button = document.createElement("button");
+  left_button.appendChild(document.createTextNode("<"));
+  left_button.className = "scl_Slider_left_button";
+  left_button.onclick = tranLeft;
+  slider.appendChild(left_button);
+
+  const image_holder = document.createElement("div");
+  image_holder.id = "" + travel.length;
+  image_holder.className = "scl_Slider_imgholder";
+  for (let i = 0; i < image_list.length; i++) {
     const img = document.createElement("img");
-    img.src = image_src;
-    img.className = "scl_ANone_img";
-    container.appendChild(img);
-    card.appendChild(container);
-    const info = document.createElement("p");
-    info.appendChild(document.createTextNode(text));
-    info.className = "scl_ANone_text";
-    card.appendChild(info);
-    const elem = document.getElementsByName(elementname);
-    elem[0].appendChild(card);
-    this.cards.push(card);
-  },
+    img.src = image_list[i];
+    img.className = "scl_Slider_img";
+    image_holder.appendChild(img);
+  }
+  slider.appendChild(image_holder);
 
-  makeCardAOpacity: function (
-    elementname,
-    image_src1,
-    image_src2,
-    title_text,
-    text,
-    links
+  const right_button = document.createElement("button");
+  right_button.appendChild(document.createTextNode(">"));
+  right_button.className = "scl_Slider_right_button";
+  right_button.onclick = tranRight;
+  slider.appendChild(right_button);
+  travel.push(0);
+
+  const elem = document.getElementsByName(elementname);
+  elem[0].appendChild(slider);
+  return slider;
+}
+
+const cards = {
+  1: makeCardANone,
+  2: makeCardAOpacity,
+  3: makeCardAFlip,
+};
+
+const sliders = {
+  1: makeSliderOne,
+};
+
+Card.prototype = {
+  makeCard: function (
+    element,
+    cardnumber = 1,
+    facenumber = 1,
+    faces = [],
+    optional = {}
   ) {
-    const card = document.createElement("div");
-    card.className = "scl_AOpacity";
-    card.style.backgroundImage = "url(" + image_src1 + ")";
-
-    const container = document.createElement("div");
-    container.className = "scl_AOpacity_container";
-    const icon = document.createElement("img");
-    icon.src = image_src2;
-    icon.className = "scl_AOpacity_icon";
-    container.appendChild(icon);
-    card.appendChild(container);
-
-    const container2 = document.createElement("div");
-    container2.className = "scl_AOpacity_container2";
-    const info = document.createElement("h1");
-    info.appendChild(document.createTextNode(title_text));
-    info.className = "scl_AOpacity_text";
-    const description = document.createElement("p");
-    description.appendChild(document.createTextNode(text));
-    description.className = "scl_AOpacity_text2";
-
-    const face_div = document.createElement("div");
-    face_div.className = "scl_AOpacity_aholder";
-
-    const facebook_a = document.createElement("a");
-    facebook_a.className = "scl_AOpacity_a";
-    facebook_a.href = links[0];
-    facebook_a.target = "_blank";
-    const facebook_icon = document.createElement("i");
-    facebook_icon.className = "fa fa-facebook-square";
-    facebook_icon.setAttribute("aria-hidden", "true");
-    facebook_a.appendChild(facebook_icon);
-    face_div.appendChild(facebook_a);
-
-    const github_a = document.createElement("a");
-    github_a.className = "scl_AOpacity_agit";
-    github_a.href = links[1];
-    github_a.target = "_blank";
-    const github_icon = document.createElement("i");
-    github_icon.className = "fa fa-github-square";
-    github_icon.setAttribute("aria-hidden", "true");
-    github_a.appendChild(github_icon);
-    face_div.appendChild(github_a);
-
-    const linked_a = document.createElement("a");
-    linked_a.className = "scl_AOpacity_agit";
-    linked_a.href = links[2];
-    linked_a.target = "_blank";
-    const linked_icon = document.createElement("i");
-    linked_icon.className = "fa fa-linkedin-square";
-    linked_icon.setAttribute("aria-hidden", "true");
-    linked_a.appendChild(linked_icon);
-    face_div.appendChild(linked_a);
-
-    container2.appendChild(info);
-    container2.appendChild(description);
-    container2.appendChild(face_div);
-    card.appendChild(container2);
-
-    const elem = document.getElementsByName(elementname);
-    elem[0].appendChild(card);
-    this.cards.push(card);
-  },
-
-  makeCardAFlip: function (
-    elementname,
-    image_src1,
-    image_src2,
-    title_text,
-    text,
-    links
-  ) {
-    const card = document.createElement("div");
-    card.className = "scl_AFlip";
-
-    const front = document.createElement("div");
-    front.className = "scl_AFlip_front";
-    front.style.backgroundImage = "url(" + image_src1 + ")";
-
-    const text_container = document.createElement("div");
-    text_container.className = "scl_AFlip_container2";
-    const info = document.createElement("h1");
-    info.appendChild(document.createTextNode(title_text));
-    info.className = "scl_AFlip_text";
-    const description = document.createElement("p");
-    description.appendChild(document.createTextNode(text));
-    description.className = "scl_AFlip_text2";
-    text_container.appendChild(info);
-    text_container.appendChild(description);
-    front.appendChild(text_container);
-
-    card.appendChild(front);
-
-    const back = document.createElement("div");
-
-    back.className = "scl_AFlip_back";
-
-    const img_container = document.createElement("div");
-    img_container.className = "scl_AFlip_container";
-    const icon = document.createElement("img");
-    icon.src = image_src2;
-    icon.className = "scl_AFlip_icon";
-    img_container.appendChild(icon);
-    back.appendChild(img_container);
-
-    const details = document.createElement("div");
-    details.className = "scl_AFlip_details";
-    const info_back = document.createElement("h1");
-    info_back.appendChild(document.createTextNode(title_text));
-    info_back.className = "scl_AFlip_text3";
-    details.appendChild(info_back);
-
-    const face_div = document.createElement("div");
-    face_div.className = "scl_AFlip_aholder";
-
-    const facebook_a = document.createElement("a");
-    facebook_a.className = "scl_AFlip_a";
-    facebook_a.href = links[0];
-    facebook_a.target = "_blank";
-    const facebook_icon = document.createElement("i");
-    facebook_icon.className = "fa fa-facebook-square";
-    facebook_icon.setAttribute("aria-hidden", "true");
-    facebook_a.appendChild(facebook_icon);
-    face_div.appendChild(facebook_a);
-
-    const github_a = document.createElement("a");
-    github_a.className = "scl_AFlip_agit";
-    github_a.href = links[1];
-    github_a.target = "_blank";
-    const github_icon = document.createElement("i");
-    github_icon.className = "fa fa-github-square";
-    github_icon.setAttribute("aria-hidden", "true");
-    github_a.appendChild(github_icon);
-    face_div.appendChild(github_a);
-
-    const linked_a = document.createElement("a");
-    linked_a.className = "scl_AFlip_agit";
-    linked_a.href = links[2];
-    linked_a.target = "_blank";
-    const linked_icon = document.createElement("i");
-    linked_icon.className = "fa fa-linkedin-square";
-    linked_icon.setAttribute("aria-hidden", "true");
-    linked_a.appendChild(linked_icon);
-    face_div.appendChild(linked_a);
-
-    const face_div2 = document.createElement("div");
-    face_div2.className = "scl_AFlip_aholder";
-
-    const twitter_a = document.createElement("a");
-    twitter_a.className = "scl_AFlip_a";
-    twitter_a.href = links[3];
-    twitter_a.target = "_blank";
-    const twitter_icon = document.createElement("i");
-    twitter_icon.className = "fa fa-twitter-square";
-    twitter_icon.setAttribute("aria-hidden", "true");
-    twitter_a.appendChild(twitter_icon);
-    face_div2.appendChild(twitter_a);
-
-    const instagram_a = document.createElement("a");
-    instagram_a.className = "scl_AFlip_agit";
-    instagram_a.href = links[4];
-    instagram_a.target = "_blank";
-    const instagram_icon = document.createElement("i");
-    instagram_icon.className = "fa fa-instagram";
-    instagram_icon.setAttribute("aria-hidden", "true");
-    instagram_a.appendChild(instagram_icon);
-    face_div2.appendChild(instagram_a);
-
-    const watsapp_a = document.createElement("a");
-    watsapp_a.className = "scl_AFlip_agit";
-    watsapp_a.href = links[5];
-    watsapp_a.target = "_blank";
-    const watsapp_icon = document.createElement("i");
-    watsapp_icon.className = "fa fa-envelope";
-    watsapp_icon.setAttribute("aria-hidden", "true");
-    watsapp_a.appendChild(watsapp_icon);
-    face_div2.appendChild(watsapp_a);
-
-    back.appendChild(details);
-    back.appendChild(face_div);
-    back.appendChild(face_div2);
-    card.appendChild(back);
-
-    const elem = document.getElementsByName(elementname);
-    elem[0].appendChild(card);
-    this.cards.push(card);
-  },
-  makeSliderOne: function (elementname, image_list) {
-    const slider = document.createElement("div");
-    slider.className = "scl_Slider";
-
-    const left_button = document.createElement("button");
-    left_button.appendChild(document.createTextNode("<"));
-    left_button.className = "scl_Slider_left_button";
-    left_button.onclick = tranLeft;
-    slider.appendChild(left_button);
-
-    const image_holder = document.createElement("div");
-    image_holder.id = "scl_Slider_imgholder";
-    image_holder.className = "scl_Slider_imgholder";
-    for (let i = 0; i < image_list.length; i++) {
-      const img = document.createElement("img");
-      img.src = image_list[i];
-      img.className = "scl_Slider_img";
-      image_holder.appendChild(img);
+    if (cardnumber === 3) {
+      const card = cards[cardnumber](
+        element,
+        facenumber[0],
+        faces[0],
+        facenumber[1],
+        faces[1]
+      );
+      this.cards.push(card);
+    } else {
+      const card = cards[cardnumber](element, facenumber, faces);
+      this.cards.push(card);
     }
-    slider.appendChild(image_holder);
-
-    const right_button = document.createElement("button");
-    right_button.appendChild(document.createTextNode(">"));
-    right_button.className = "scl_Slider_right_button";
-    right_button.onclick = tranRight;
-    slider.appendChild(right_button);
-
-    const elem = document.getElementsByName(elementname);
-    elem[0].appendChild(slider);
-    this.cards.push(slider);
+  },
+  makeSlider: function (element, slidernumber, inputs, facenumber = []) {
+    if (facenumber.length === 0) {
+      const slider = sliders[slidernumber](element, inputs);
+      this.slider.push(slider);
+    }
   },
 };
